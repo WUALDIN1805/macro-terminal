53fc04eda692eb312996d71ec5958a6fa41b84c5
wualdin javier salas <127115065+WUALDIN1805@users.noreply.github.com>
2026-03-22 18:27:17 +0000

    Bypass final

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 // @ts-nocheck
 "use client";
 
+
+
 import { useState } from "react";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
